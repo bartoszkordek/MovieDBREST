1f28c670a56ae6de964ac50bc8db0434d3dc4a67
Bartosz Kordek <bartosz.kordek10@gmail.com>
2025-12-17 23:38:20 +0100

    Fix geocode

--- routers/geocode.py
+++ routers/geocode.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, FastAPI
+from fastapi import APIRouter, FastAPI, HTTPException
 import requests
 
 router = APIRouter(
@@ -8,10 +8,16 @@ router = APIRouter(
 
 app = FastAPI()
 
-@router.get("/")
+
+@router.get("")
 def geocode(lat: float, lon: float):
     url = f"https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat={lat}&lon={lon}"
-    response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
-    return response.json()
+
+    response = requests.get(url, headers={"User-Agent": "MovieDB_Educational_App/1.0"})
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise HTTPException(status_code=response.status_code, detail="Cannot pull geolocalization data.")
+
 
 app.include_router(router)
